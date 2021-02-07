c024597f4641b79e20feb0489a04b32fbb3f012d
deeprajbasu <deepraj.basu@gmail.com>
2021-02-07 14:00:37 +0530

    try

---
+++ raycast/Assets/Scenes/camRayTerrain.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camRayTerrain : MonoBehaviour
+{
+    public Transform placed;
+    public Camera cam;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            //create a ray from camera to mouse position
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit h; 
+
+        if (Physics.Raycast(ray,out h)){
+            // if ray hits anything draw greenline 
+            placed.position=h.point;
+        }
+    
+    
+    }
+}
