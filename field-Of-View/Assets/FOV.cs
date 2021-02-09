c83b94a89a06ee569f0d8ff9540ef62ead04b6c8
deeprajbasu <deepraj.basu@gmail.com>
2021-02-09 14:21:58 +0530

    got view pooints in viewable points within view region by raycasting

--- field-Of-View/Assets/FOV.cs
+++ field-Of-View/Assets/FOV.cs
@@ -66,16 +66,49 @@ public class FOV : MonoBehaviour {
 		//cast rays to highlight entire FOV region
 		int numrays =Mathf.RoundToInt(viewAngle*meshRes) ;// round to whole number 
 		float raystepSize = viewAngle/numrays;//angle stepsize for subsequent rays. 
-		
-		for(int i =0; i <= numrays;i++){
-			float angle = transform.eulerAngles.y - viewAngle/2+raystepSize*i;
-			Debug.DrawLine(transform.position,transform.position+direction(angle,true)*viewRadius,Color.green);
+
+		list<Vector3> viewPoints = new list<Vector3>();//store visible points after view region raycasting 
+
+		//cast view rays in the view region and get their info
+		for(int i =0; i <= numrays;i++){//for every ray in the view region
+			// Vector3 viewAngleA = fow.direction (-fow.viewAngle / 2, false);
+			// remember we used something similar to figure out +- limits of players view according to view angle,
+			//using our direction method  
+
+
+			float angle = transform.eulerAngles.y - viewAngle/2 + (raystepSize*i) ;
+			//figure out ray angle for ith ray
+			
+			viewRayInfo viewRay = new viewCast(angle);//cast and capture ith ray info
+			viewPoints.Add(viewRay.point);//add end point of ray to list of view points.
+
 
 		}
+		//once we have the view points in the view region 
+		//we can try to create a triangulated mesh to better visualize the field of view
 
 
 
 	}
+	viewRayInfo viewCast(float angl){  //casts rays in the view region
+		Vector3 dir = direction(angl);
+		// get direction of ray to be cast
+
+		RaycastHit hit; //if ray hit something
+		if (Physics.Raycast(transform.position,dir,out hit,viewRadius,obstacleMask)){
+			//if ray cast from position till view radius in view region hits any wall 
+			//return ray info as our custom datastructure
+			
+			return new viewRayInfo(true,hit.point,hit.distance,angle);
+			
+		}else{
+			return new viewRayInfo(false, transform.position + dir * viewRadius, viewRadius, angl);
+			//return whole lenght ray if not hit anything
+
+		}
+
+	}
+	
 	public Vector3 direction(float angleInDegrees, bool angleIsGlobal) {
 		//output direction vector from view angle degrees
 		//used for on screen visualization 
@@ -84,4 +117,19 @@ public class FOV : MonoBehaviour {
 		}
 		return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad),0,Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
 	}
-}
+
+
+	public struct viewRayInfo {//will store info about cast rays in the view region.
+		public bool hit;//if ray hit anything. 
+		public Vector3 point;//point of collision
+		public float dst;//distance of collision 
+		public float angle;// angle at which ray was cast 
+
+		public viewRayInfo(bool _hit, Vector3 _point, float _dst, float _angle) {
+			hit = _hit;
+			point = _point;
+			dst = _dst;
+			angle = _angle;
+			}
+		}	
+	}
